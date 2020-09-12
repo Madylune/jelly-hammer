d158b404ba4cbcde1cc4de4fe39d4d43f49de9ec
Julie Nginn <nginn.julie@gmail.com>
2020-09-12 16:35:20 +0200

    Feat: Create character script

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    protected Animator animator;
+
+    protected SpriteRenderer spriteRenderer;
+
+    protected float moveSpeed = 3f;
+
+    protected bool isDead = false;
+
+    protected Vector3 CheckPointPosition;
+
+    public virtual void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        CheckPointPosition = transform.position;
+    }
+
+    public void Flip(float velocity)
+    {
+        if (velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    public void RespawnAtCheckpoint()
+    {
+        transform.position = CheckPointPosition;
+        isDead = false;
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,8 +1,88 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Character
 {
-    
+    private static Player instance;
+
+    public static Player MyInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<Player>();
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    private bool isAttacking;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();
+        Attack();
+        ResetValues();
+    }
+
+    private void Walk()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        Vector2 inputVector = new Vector2(inputX, inputY);
+        Vector2 movement = inputVector * moveSpeed;
+
+
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+        }
+
+        Flip(movement.x);
+
+        if (movement.x == 0 && movement.y == 0)
+        {
+            animator.SetBool("IsWalking", false);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", true);
+        }
+    }
+
+    private void Attack()
+    {
+        if (isAttacking && !animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            animator.SetTrigger("Attack");
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isAttacking = true;
+        }
+    }
+
+    private void ResetValues()
+    {
+        isAttacking = false;
+    }
 }
