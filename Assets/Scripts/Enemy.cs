aaf8a5377b3d90cdb0b94663988d42b6054dc33f
Julie Nginn <nginn.julie@gmail.com>
2020-09-12 18:24:38 +0200

    Feat: Adding patrol AI to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,42 @@ using UnityEngine;
 
 public class Enemy : Character
 {
+    [SerializeField]
+    private Transform moveSpot;
+
+    [SerializeField]
+    private float minX, maxX, minY, maxY;
+
+    [SerializeField]
+    private float startWaitTime;
+
+    private float waitTime;
+
+    private float speed = 1f;
+
     public override void Start()
     {
         base.Start();
+
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
 }
