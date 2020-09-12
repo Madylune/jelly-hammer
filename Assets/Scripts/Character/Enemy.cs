2cb2c8505bbab29946ca2bf884d3929c556f16dc
Julie Nginn <nginn.julie@gmail.com>
2020-09-12 19:32:08 +0200

    Feat: Spawn enemies + fix enemy move spot

---
+++ Assets/Scripts/Character/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character
+{
+    private Vector2 moveSpot;
+
+    [SerializeField]
+    private float minX, maxX, minY, maxY;
+
+    [SerializeField]
+    private float startWaitTime;
+
+    private float waitTime;
+
+    private float speed = 1f;
+
+    public float MyMinX { get => minX; }
+    public float MyMaxX { get => maxX; }
+    public float MyMinY { get => minY; }
+    public float MyMaxY { get => maxY; }
+
+    public override void Start()
+    {
+        base.Start();
+
+        waitTime = startWaitTime;
+        moveSpot = new Vector2(Random.Range(MyMinX, MyMaxX), Random.Range(MyMinY, MyMaxY));
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot, speed * Time.deltaTime);
+
+        if (transform.position.x > moveSpot.x)
+        {
+            Flip(1f);
+        }
+        else if (transform.position.x < moveSpot.x)
+        {
+            Flip(-1f);
+        }
+
+        if (Vector2.Distance(transform.position, moveSpot) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot = new Vector2(Random.Range(MyMinX, MyMaxX), Random.Range(MyMinY, MyMaxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
